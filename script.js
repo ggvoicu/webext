dd698c18b40f48c8b09d8b752512027d95eccc49
George Voicu <george.voicu@P6238.svdomain1.softvision.ro>
2017-09-21 12:31:18 +0300

    Exercise 1

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 var allImg = document.getElementsByTagName("IMG");
 var replacePic = browser.extension.getURL("picture.jpg");
-
-function activatePics(){
-	console.log("test");
-	
-	for ( var i=0; i < allImg.length; i++){
-	allImg[i].src = replacePic;
-	}
+for ( var i=0; i < allImg.length; i++){
+allImg[i].src = replacePic;
 }
-
