d4bee41b396e16e9bcf3aec33afe262c3d5f7c87
George Voicu <george.voicu@P6238.svdomain1.softvision.ro>
2017-09-22 15:50:37 +0300

    Remake-Exercises

--- script.js
+++
@@ -1,5 +0,0 @@
-var allImg = document.getElementsByTagName("IMG");
-var replacePic = browser.extension.getURL("picture.jpg");
-for ( var i=0; i < allImg.length; i++){
-allImg[i].src = replacePic;
-}
